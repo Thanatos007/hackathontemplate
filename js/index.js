94f5cb15a841c0f79213353931222edf2c497ca1
James Do <james.s.do@gmail.com>
2015-12-09 16:41:00 -0500

    Pushing the actual template.

---
+++ js/index.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+	// this is the code that runs when the
+	// screen loads
+
+};
+
+function exampleFunction() {
+	// this is an example function with no parameters
+
+}
+
